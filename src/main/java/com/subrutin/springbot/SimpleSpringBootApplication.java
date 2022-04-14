8ca1e909f8c898ac78507636b5bdd392e4a5c4ed
subhan-sd <subhan.singandaru2@gmail.com>
2022-04-14 14:26:17 +0700

    Perubahan Error (Update Maven from 3.6.4 to 3.8.5)

--- src/main/java/com/subrutin/springbot/SimpleSpringBootApplication.java
+++ src/main/java/com/subrutin/springbot/SimpleSpringBootApplication.java
@@ -2,11 +2,14 @@ package com.subrutin.springbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SimpleSpringBootApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SimpleSpringBootApplication.class, args);
 	}
 
--- src/main/java/com/subrutin/springbot/controllers/WelcomeController.java
+++ src/main/java/com/subrutin/springbot/controllers/WelcomeController.java
@@ -1,10 +1,10 @@
 package com.subrutin.springbot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.HashMap;
+//import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/welcome")
@@ -12,17 +12,13 @@ public class WelcomeController {
 
     @GetMapping
     public String welcome(){
-        HashMap<String, String> capitalCities = new HashMap<String, String>();
-        capitalCities.put("id", "123");
-        capitalCities.put("name", "ABC");
-        capitalCities.put("amount", "100");
-        System.out.println(capitalCities);
+//        HashMap<String, String> capitalCities = new HashMap<String, String>();
+//        capitalCities.put("id", "123");
+//        capitalCities.put("name", "ABC");
+//        capitalCities.put("amount", "100");
+//        System.out.println(capitalCities);
 
         return "Test Dulu Error";
     };
 
-//    @PostMapping
-//    public String others(){
-//        return "Another Data";
-//    }
 }
